723e08dee154766ba67037691d48222ef43893fd
matheus-fsantossnc09 <matheus.fsantos72@senacsp.edu.br>
2025-03-13 23:06:03 -0300

    A pratica leva a excelencia

--- Colocando_em_pratica/Form1.Designer.cs
+++ Colocando_em_pratica/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Colocando_em_pratica
 {
-    partial class Form1
+    partial class frm_Revisao
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,107 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lbl_Texto = new System.Windows.Forms.Label();
+            this.txt_Texto = new System.Windows.Forms.TextBox();
+            this.btn_Mudar = new System.Windows.Forms.Button();
+            this.btn_Voltar = new System.Windows.Forms.Button();
+            this.btn_Sair = new System.Windows.Forms.Button();
+            this.btn_Proximo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_Texto
+            // 
+            this.lbl_Texto.AutoSize = true;
+            this.lbl_Texto.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Texto.Location = new System.Drawing.Point(106, 83);
+            this.lbl_Texto.Name = "lbl_Texto";
+            this.lbl_Texto.Size = new System.Drawing.Size(102, 39);
+            this.lbl_Texto.TabIndex = 0;
+            this.lbl_Texto.Text = "Texto";
+            this.lbl_Texto.Click += new System.EventHandler(this.lbl_Texto_Click);
+            // 
+            // txt_Texto
+            // 
+            this.txt_Texto.Location = new System.Drawing.Point(113, 148);
+            this.txt_Texto.Name = "txt_Texto";
+            this.txt_Texto.Size = new System.Drawing.Size(493, 22);
+            this.txt_Texto.TabIndex = 1;
+            this.txt_Texto.TextChanged += new System.EventHandler(this.txt_Texto_TextChanged);
+            // 
+            // btn_Mudar
+            // 
+            this.btn_Mudar.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Mudar.Location = new System.Drawing.Point(113, 210);
+            this.btn_Mudar.Name = "btn_Mudar";
+            this.btn_Mudar.Size = new System.Drawing.Size(124, 55);
+            this.btn_Mudar.TabIndex = 2;
+            this.btn_Mudar.Text = "Mudar";
+            this.btn_Mudar.UseVisualStyleBackColor = true;
+            this.btn_Mudar.Click += new System.EventHandler(this.btn_Mudar_Click);
+            // 
+            // btn_Voltar
+            // 
+            this.btn_Voltar.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Voltar.Location = new System.Drawing.Point(482, 210);
+            this.btn_Voltar.Name = "btn_Voltar";
+            this.btn_Voltar.Size = new System.Drawing.Size(124, 55);
+            this.btn_Voltar.TabIndex = 3;
+            this.btn_Voltar.Text = "Voltar";
+            this.btn_Voltar.UseVisualStyleBackColor = true;
+            this.btn_Voltar.Click += new System.EventHandler(this.btn_Voltar_Click);
+            // 
+            // btn_Sair
+            // 
+            this.btn_Sair.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Sair.Location = new System.Drawing.Point(113, 487);
+            this.btn_Sair.Name = "btn_Sair";
+            this.btn_Sair.Size = new System.Drawing.Size(146, 47);
+            this.btn_Sair.TabIndex = 4;
+            this.btn_Sair.Text = "Sair";
+            this.btn_Sair.UseVisualStyleBackColor = true;
+            this.btn_Sair.Click += new System.EventHandler(this.btn_Sair_Click);
+            // 
+            // btn_Proximo
+            // 
+            this.btn_Proximo.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Proximo.Location = new System.Drawing.Point(471, 487);
+            this.btn_Proximo.Name = "btn_Proximo";
+            this.btn_Proximo.Size = new System.Drawing.Size(135, 47);
+            this.btn_Proximo.TabIndex = 5;
+            this.btn_Proximo.Text = "Próximo";
+            this.btn_Proximo.UseVisualStyleBackColor = true;
+            this.btn_Proximo.Click += new System.EventHandler(this.btn_Proximo_Click);
+            // 
+            // frm_Revisao
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(966, 693);
+            this.Controls.Add(this.btn_Proximo);
+            this.Controls.Add(this.btn_Sair);
+            this.Controls.Add(this.btn_Voltar);
+            this.Controls.Add(this.btn_Mudar);
+            this.Controls.Add(this.txt_Texto);
+            this.Controls.Add(this.lbl_Texto);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "frm_Revisao";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Revisão";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbl_Texto;
+        private System.Windows.Forms.TextBox txt_Texto;
+        private System.Windows.Forms.Button btn_Mudar;
+        private System.Windows.Forms.Button btn_Voltar;
+        private System.Windows.Forms.Button btn_Sair;
+        private System.Windows.Forms.Button btn_Proximo;
     }
 }
 
---
+++ Colocando_em_pratica/frm_ex02.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Colocando_em_pratica
+{
+    public partial class frm_ex02: Form
+    {
+        public frm_ex02()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Anterior_Click(object sender, EventArgs e)
+        {
+            frm_Revisao frm_Revisao = new frm_Revisao();
+            frm_Revisao.Show();
+            this.Hide();
+        }
+
+        private void btn_Soma_Click(object sender, EventArgs e)
+        {
+            double num1 = Convert.ToDouble(txt_Valor1.Text);
+            double num2 = Convert.ToDouble(txt_Valor2.Text);
+            double resultadoSoma = num1 + num2;
+
+            lbl_Resultado.Text = resultadoSoma.ToString();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double num1 = Convert.ToDouble(txt_Valor1.Text);
+            double num2 = Convert.ToDouble(txt_Valor2.Text);
+            double resultadoDivisao = num1 / num2;
+
+            lbl_Resultado.Text = resultadoDivisao.ToString();
+
+        }
+
+        private void btn_Subtracao_Click(object sender, EventArgs e)
+        {
+            double num1 = Convert.ToDouble(txt_Valor1.Text);
+            double num2 = Convert.ToDouble(txt_Valor2.Text);
+            double resultadoSubtracacao = num1 - num2;
+
+            lbl_Resultado.Text = resultadoSubtracacao.ToString();
+        }
+
+        private void btn_Multiplicacao_Click(object sender, EventArgs e)
+        {
+            double num1 = Convert.ToDouble(txt_Valor1.Text);
+            double num2 = Convert.ToDouble(txt_Valor2.Text);
+            double resultadoMultiplicacao = num1 * num2;
+
+            lbl_Resultado.Text = resultadoMultiplicacao.ToString();
+        }
+    }
+
+}
